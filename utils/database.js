d9c8b9520e59f9780f94c6019191b3a6472932a2
karanvir <karanvirvirdi186@gmail.com>
2021-04-07 11:33:21 +0530

    asad

--- utils/database.js
+++ utils/database.js
@@ -4,7 +4,7 @@ require('dotenv').config()
 const MongoClient = require('mongodb').MongoClient;
 
 const mongoConnect = (callback) =>{
-    MongoClient.connect(`mongodb+srv://shoppingUser:9779kanty@shoppingcart.vzgsg.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`)
+    MongoClient.connect(`mongodb+srv://${process.env.DB_USER1}:${process.env.DB_PASSWORD1}@shoppingcart.vzgsg.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`)
     .then(client =>{
         console.log('connected');
         callback(client)
