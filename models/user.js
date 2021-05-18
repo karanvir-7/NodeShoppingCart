8d8458d5ecd472537f320b64826e52ac8f61588c
karanvir <karanvirvirdi186@gmail.com>
2021-05-19 00:07:19 +0530

    validaion started

--- models/user.js
+++ models/user.js
@@ -4,7 +4,8 @@ const bcrypt = require('bcryptjs');
 const userSchema = new mongoose.Schema({
   name:{
     type: String,
-    required:true
+    required:true,
+    
   },
   email:{
     type: String,
